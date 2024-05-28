abe05fb5fcb7346d36f74b5f5d6f37f2de133529
Abdulaleem Mughal <mughalabdulaleem8@gmail.com>
2024-05-28 12:16:46 -0900

    Complete

--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -1,19 +1,13 @@
 import { API_KEY } from "../utils/contrant";
 import cloud from "../Assets/cloud.png";
-import clear from "../Assets/clear.png";
-import drizzle from "../Assets/drizzle.png";
-import rain from "../Assets/rain.png";
-import snow from "../Assets/snow.png";
 import humidity from "../Assets/humidity.png";
 import wind from "../Assets/wind.png";
 import { useDispatch, useSelector } from "react-redux";
 import { changeWeather } from "../utils/weatherSlice";
-import { useState } from "react";
 
 const WeatherApp = () => {
   const dispatch = useDispatch();
-  const user = useSelector((store) => store.weather);
-  const { wicon, setWicon } = useState(cloud);
+  const user = useSelector((store) => store?.weather?.weather);
 
   const search = async () => {
     const element = document.getElementsByClassName("cityInput");
@@ -22,34 +16,12 @@ const WeatherApp = () => {
       return 0;
     }
 
-    const weather_API = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${API_KEY}`;
-
-    const data = await fetch(weather_API);
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${API_KEY}`);
+    // const data = await fetch(weather_API);
     const json = await data.json();
 
     dispatch(changeWeather(json));
 
-    // if (
-    //   user?.weather?.weather?.weather[0]?.icon === "01d" ||
-    //   user?.weather?.weather?.weather[0]?.icon === "01n"
-    // ) {
-    //   setWicon(clear);
-    // } else if (
-    //   user?.weather?.weather?.weather[0]?.icon === "10d" ||
-    //   user?.weather?.weather?.weather[0]?.icon === "10n"
-    // ) {
-    //   setWicon(rain);
-    // } else if (
-    //   user?.weather?.weather?.weather[0]?.icon === "09d" ||
-    //   user?.weather?.weather?.weather[0]?.icon === "09n"
-    // ) {
-    //   setWicon(drizzle);
-    // } else if (
-    //   user?.weather?.weather?.weather[0]?.icon === "13d" ||
-    //   user?.weather?.weather?.weather[0]?.icon === "13n"
-    // ) {
-    //   setWicon(snow);
-    // }
   };
 
   return (
@@ -64,7 +36,7 @@ const WeatherApp = () => {
                 search();
               }}
             >
-              <ion-icon name="search-outline"></ion-icon>
+              Search
             </div>
           </div>
 
@@ -72,14 +44,14 @@ const WeatherApp = () => {
             <img src={cloud} />
           </div>
 
-          <div className="weather-temp">{user?.weather?.main?.temp}°C</div>
-          <div className="weather-location">{user?.weather?.name}</div>
+          <div className="weather-temp">{Math.floor(user?.main?.temp)}°C</div>
+          <div className="weather-location">{user?.name}</div>
           <div className="data-container">
             <div className="element">
               <img src={humidity} className="icon" />
               <div className="data">
                 <div className="humidity-percentage">
-                  {user?.weather?.main?.humidity}%
+                  {user?.main?.humidity}%
                 </div>
                 <div className="text">Humidity</div>
               </div>
@@ -89,7 +61,7 @@ const WeatherApp = () => {
               <img src={wind} className="icon" />
               <div className="data">
                 <div className="wind-rate">
-                  {user?.weather?.wind?.speed}km/h
+                  {Math.floor(user?.wind?.speed)}km/h
                 </div>
                 <div className="text">Wind Speed</div>
               </div>
